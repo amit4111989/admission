e434cd03603f29230abb4a22bfe10a81a9795eae
Amit Juneja <amit.juneja004@gmail.com>
2015-03-07 02:36:41 -0700

    partials added

--- admission/app.js
+++ admission/app.js
@@ -22,6 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
