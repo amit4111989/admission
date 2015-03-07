433f8f7f47eb7ade286b4f73019741b35f4734df
Amit Juneja <amit.juneja004@gmail.com>
2015-03-07 02:25:48 -0700

    paritals syntax added

--- admission/routes/index.js
+++ admission/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Admission' });
+  res.render('index', { title: 'Admission' ,   
+      partials:
+      {
+        part: 'error',
+      }
+    });
 });
 
 module.exports = router;
